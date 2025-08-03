aa01b38337d1b69dca975560370315fe1c5c82ca
Harry Awanda <harrywanda0704@gmail.com>
2025-08-03 17:01:37 +0700

    add aside layouts blade template

---
+++ resources/views/layouts/aside.blade.php
@@ -0,0 +1,27 @@
+<!-- Menu -->
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+  <div class="app-brand demo ">
+    <a href="" class="app-brand-link">
+      <span class="app-brand-logo demo">
+        <img src="{{ asset('assets/img/silap.png') }}" width="50px" alt="">
+      </span>
+      <span class="app-brand-text demo menu-text fw-bold ms-2">SILAP</span>
+    </a>
+    <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+      <i class="bx bx-chevron-left bx-sm align-middle"></i>
+    </a>
+  </div>
+
+  <div class="menu-inner-shadow"></div>
+  
+  <ul class="menu-inner py-1">
+    <!-- Dashboards -->
+    <li class="menu-item active">
+      <a href="" class="menu-link">
+        <i class="menu-icon tf-icons bx bx-home-circle"></i>
+        Dashboards
+      </a>
+    </li>
+  </ul>
+</aside>
+<!-- / Menu -->
