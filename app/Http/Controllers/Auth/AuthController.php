89aa3efbe22cee2189efd7f177b8091268e7975d
Harry Awanda <harrywanda0704@gmail.com>
2025-09-05 11:44:31 +0700

    feat(auth): buat AuthController (skeleton)

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller {
+    // Method untuk menampilkan form login
+  public function showLoginForm() {
+    //
+  }
+
+  // Method untuk menangani proses login
+  public function postLogin(Request $request) {
+    //
+  }
+
+  // Method untuk logout
+  public function logout(Request $request) {
+    //
+  }
+}
