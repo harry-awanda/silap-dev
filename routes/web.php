586dbff4130be9cc747eeca7005f95b9f726cd0e
Harry Awanda <harrywanda0704@gmail.com>
2025-09-07 16:56:10 +0700

    feat(route): terapkan auth & checkRole pada route dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\{
+    DashboardController,
+};
+
 use App\Http\Controllers\Auth\{
     AuthController,
 };
@@ -11,4 +16,11 @@ Route::controller(AuthController::class)->group(function() {
   Route::get('auth/login', 'showLoginForm')->name('login')->middleware('guest'); // Perbarui URL
   Route::post('auth/login', 'postLogin')->name('postLogin'); // Gunakan route name untuk post login
   Route::post('logout', 'logout')->name('logout')->middleware('auth');
+});
+
+// Routes with common 'auth' middleware
+Route::middleware(['protected'])->group(function () {
+  Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard')
+  ->middleware('checkRole:admin,guru,guru_piket,guru_bk,siswa');
+  
 });
